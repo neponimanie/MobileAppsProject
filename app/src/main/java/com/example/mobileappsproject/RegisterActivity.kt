4f1c90249ecb8b0cd35b62c632d24e2336920514
neponimanie <andrey.phon@gmail.com>
2024-12-01 11:32:51 +0100

    Onboarding navigation

---
+++ app/src/main/java/com/example/mobileappsproject/RegisterActivity.kt
@@ -0,0 +1,27 @@
+package com.example.mobileappsproject
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class RegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_register)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        val sign_in = findViewById<TextView>(R.id.sign_in)
+        sign_in.setOnClickListener {
+            val go_to_sign_in = Intent(this,SignInActivity::class.java)
+            startActivity(go_to_sign_in)
+        }
+    }
+}
--- app/src/main/java/com/example/mobileappsproject/SignInActivity.kt
+++ app/src/main/java/com/example/mobileappsproject/SignInActivity.kt
@@ -1,12 +1,14 @@
 package com.example.mobileappsproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +18,11 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        val register = findViewById<TextView>(R.id.newmember)
+        register.setOnClickListener {
+            val go_to_registration = Intent(this,RegisterActivity::class.java)
+            startActivity(go_to_registration)
+        }
+
     }
 }
