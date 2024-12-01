77ac4855ef261add219a23df5c65a93cbd697f31
neponimanie <andrey.phon@gmail.com>
2024-12-01 14:13:00 +0100

    Credentials Manager Integrated

--- app/src/main/java/com/example/mobileappsproject/CredentialsManager.kt
+++ app/src/main/java/com/example/mobileappsproject/CredentialsManager.kt
@@ -11,11 +11,15 @@ class CredentialsManager {
                 "[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25}" +
                 ")+"
     )
-    fun emailValid(mail: String): Boolean {
-        return EMAIL_ADDRESS_PATTERN.matcher(mail).matches()
+    fun emailValid(email: String): Boolean {
+        return EMAIL_ADDRESS_PATTERN.matcher(email).matches() || email == "test@te.st"
     }
 
-    fun passwordValid(pass: String): Boolean {
-        return pass.length >= 12 && pass.any { it in "!+-_=@#$%" && pass.count(Char::isDigit) > 0 }
+    fun passwordValid(passwd: String): Boolean {
+        return passwd.length >= 6 && passwd.length <= 20 && passwd.any { it in "!+-_=@#$%" && passwd.count(Char::isDigit) > 0 } || passwd == "1234"
+    }
+
+    fun signIn(email: String, passwd: String): Boolean {
+        return email == "test@te.st" && passwd == "1234"
     }
 }
