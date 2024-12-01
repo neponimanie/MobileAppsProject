01f578e2288e99484e564992e75f443bf3850caa
Lemon049 <yywmail5@gmail.com>
2024-12-01 22:07:10 +0100

    Registration - added accounts to CredentialsManager + PR

--- app/src/test/java/com/example/mobileappsproject/CredentialsManagerTest.kt
+++ app/src/test/java/com/example/mobileappsproject/CredentialsManagerTest.kt
@@ -27,4 +27,23 @@ class CredentialsManagerTest {
         val passwd = "aadpi3421_er"
         assertTrue(credentialsmanager.passwordValid(passwd))
     }
+    @Test
+    fun successfulRegistration() {
+        assertTrue(credentialsmanager.register("example@gmail.com", "1234"))
+    }
+
+    @Test
+    fun duplicateEmail() {
+        assertFalse(credentialsmanager.register("test@te.st", "new_password"))
+    }
+
+    @Test
+    fun successfulLogin() {
+        assertTrue(credentialsmanager.signIn("test@te.st", "1234"))
+    }
+
+    @Test
+    fun incorrectPassword() {
+        assertFalse(credentialsmanager.signIn("test@te.st", "wrong_password"))
+    }
 }
