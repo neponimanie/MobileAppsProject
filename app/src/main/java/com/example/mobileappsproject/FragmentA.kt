94349083c4112bd4054adc69a04590365198f166
neponimanie <andrey.phon@gmail.com>
2024-12-08 21:47:40 +0100

    Fragments Sample

---
+++ app/src/main/java/com/example/mobileappsproject/FragmentA.kt
@@ -0,0 +1,7 @@
+package com.example.mobileappsproject
+
+import androidx.fragment.app.Fragment
+
+class FragmentA: Fragment(R.layout.activity_fragment) {
+
+}
---
+++ app/src/main/java/com/example/mobileappsproject/FragmentB.kt
@@ -0,0 +1,11 @@
+package com.example.mobileappsproject
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.Fragment
+
+class   FragmentB: Fragment(R.layout.activity_fragment_b) {
+}
